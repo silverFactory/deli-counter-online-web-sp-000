ea07accb7eefcb118af74ffc571aa1bf7871cc60
Will Studabaker <will.studabaker@gmail.com>
2020-10-27 17:58:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,9 @@ def line (array)
   if array.length == 0
     puts "The line is currently empty."
   else
+    names_nums = array.map.with_index do |name, i|
+      "#{i}. #{name}"
+    end
     name_string = array.join(" ")
     puts "The line is currently: #{name_string}"
   end
