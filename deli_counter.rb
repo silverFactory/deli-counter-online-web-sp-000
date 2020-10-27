2ace59957d8e46721463e6952d3c99511db35b15
Will Studabaker <will.studabaker@gmail.com>
2020-10-27 18:02:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,5 +13,5 @@ end
 
 def take_a_number(line, name)
   line << name
-  puts "Welcome, #{name}. You are number #{line.index(name)}"
+  puts "Welcome, #{name}. You are number #{line.index(name)+1}"
 end
