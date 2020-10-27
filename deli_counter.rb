2def5bba5c048ef2546a1348b2b5c16a71c23b41
Will Studabaker <will.studabaker@gmail.com>
2020-10-27 17:45:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,8 @@
 katz_deli = []
 def line (array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{array.each.with_index {|name, i| "#{i}. #{name} "}}"
+  end
 end
